24ea251e449fcfaa360a675aabf8d936d7833b25
Hesbon Kiptoo <hesbonkiptoo5600@gmail.com>
2025-03-27 14:32:15 +0300

    feat(templates): update external resource links to use new domain for consistency and reliability chore(templates): remove unused chat components from hotel-related templates to streamline code and improve maintainability fix(urls.py): add redirect from root URL to the hotels app for better user experience

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('apps/v1/', include('app.apps.urls')),
-
+    path("", RedirectView.as_view(url="/apps/v1/hotels/", permanent=True)),
+    path("admin/", admin.site.urls),
+    path("apps/v1/", include("app.apps.urls")),
 ]
